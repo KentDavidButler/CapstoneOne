e13490a7247c39f43beed4e90150465ba0a21f80
Kent Butler <KentDavidButler@gmail.com>
2019-01-18 21:42:05 -0500

    More efficent version, built around string arrays and not an individual string

--- CapstoneOne/Program.cs
+++ CapstoneOne/Program.cs
@@ -10,32 +10,57 @@ namespace CapstoneOne
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Pig Latin Translator!");
             do
             {
-                string[] word = ValidWord();
-                Console.WriteLine("Welcome to the Pig Latin Translator!");
-                Console.WriteLine("  ");
-                word = PigLatin(word);
-                Console.WriteLine(word.ToString());
+                string[] words, ordsay;
+                string input;
+
+                Console.Write("Please type a word or sentance to translate: ");
+                input = Console.ReadLine();
+                words = StringSplit(input, out bool empty);
+                if (empty)
+                {
+                    Console.WriteLine("You need to enter something!");
+                }
+                else
+                {
+                    ordsay = PigLatin(words);
+                    for (int i = 0; i < ordsay.Length; i++)
+                    {
+                        Console.WriteLine(ordsay[i]);
+                    }
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        Console.WriteLine(words[i]);
+                    }
+                }
+
                 Console.WriteLine(" ");
                 Console.Write("Would you like to run Pig Latin Translator again? ");
             } while (YesOrNo());
 
-
+            Console.WriteLine(" ");
+            Console.WriteLine("Goodbye!");
         }
+
 
         private static string[] PigLatin(string[] word)
         {
             for (int i = 0; i < word.Length; i++)
             {
-                int firstVowel = VowelLocation(word[i]);
-                if (firstVowel == 0)
+                int firstVowel = VowelLocation(word[i].ToLower());
+                if (HasNonLetter(word[i]) && !IsContraction(word[i]))
+                {
+                    word[i] = word[i];
+                }
+                else if (firstVowel == 0)
                 {
                     word[i] = word[i] + "way";
                 }
                 else
                 {
-                    string wordManip = word[i].ToLower();
+                    string wordManip = word[i];
                     char[] stringArray = wordManip.ToCharArray();
                     word[i] = wordManip.Substring(firstVowel) + wordManip.Substring(0, firstVowel) + "ay";
                 }
@@ -43,9 +68,37 @@ namespace CapstoneOne
             return word;
         }
 
+        private static string[] StringSplit(string input, out bool empty)
+        {
+            string[] words = input.Split(' ');
+            words = words.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            //removes null and empty locations in array
+            if(words.Length==0)
+            {
+                empty = true;
+                return words;
+            }
+            empty = false;
+            return words;
+        }
+
         private static bool HasNonLetter(string word)
         {
-            return word.All(Char.IsLetter);
+            return !(word.All(Char.IsLetter));
+        }
+
+        private static bool IsContraction(string word)
+        {
+            char[] stringArray = word.ToCharArray();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if(stringArray[i] == '\'')
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private static int VowelLocation(string word)
@@ -65,30 +118,6 @@ namespace CapstoneOne
                 location++;
             }
             return -1; //something went wrong
-        }
-        public static string[] ValidWord()
-        {
-            bool validInput;
-            string[] strArray;
-            String input;
-            int wordAmmount;
-
-            Console.Write("Enter a statement or word to be Translated: ");
-            input = Console.ReadLine();
-            strArray = input.Split(' ');
-            wordAmmount = strArray.Count();
-
-            for (int i = 0; i < wordAmmount; i++)
-            {
-                validInput = HasNonLetter(strArray[i]);
-                if (!validInput)
-                {
-                    Console.Write("Please type a word without special characters or numbers: ");
-                    break;
-                }
-            }
-            return strArray;
-
         }
 
         private static bool YesOrNo()
